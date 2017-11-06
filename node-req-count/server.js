694fdf4c365be2550064cd8285a0be2bec485cc8
shawnhr <shawnk723@live.com>
2017-11-06 10:00:41 -0800

    sa04

--- node-req-count/server.js
+++ node-req-count/server.js
@@ -1,6 +1,7 @@
 var url = require('url');
 var http = require('http');
 var path = require('path');
+var fs = require('fs');
 
 var globalCounter = {};
 
@@ -10,8 +11,27 @@ var server = http.createServer(function(request, response) {
 
   if (request.method === 'POST') {
     // YOUR CODE HERE
+    console.log(path);
+    fs.readFile(endpoint, function(err, data){
+      console.log(data);
+      if(err){
+        throw err;
+      }
+      globalCounter[dogs] = 1;
+      res.end(globalCounter);
+    })
+
   } else if (request.method === 'GET') {
     // YOUR CODE HERE
+      console.log(path);
+    fs.readFile(endpoint, function(err, data){
+      console.log(data);
+      if(err){
+        throw err;
+      }
+      globalCounter[dogs] = data;
+      res.end(globalCounter);
+    })
   } else {
     response.statusCode = 404;
     response.end();
@@ -20,3 +40,5 @@ var server = http.createServer(function(request, response) {
 
 // Do not edit this line
 module.exports = server;
+
+
