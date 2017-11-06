77b11fc492ddb21bf092f097c1e0e5727a0e917b
shawnhr <shawnk723@live.com>
2017-11-06 11:13:31 -0800

    pushing missing async file

--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -12,9 +12,21 @@ var getWordCount = function(filePath, callback) {
     callback(null, wordCount);
   });
 };
-
+var filePath1 = '../async-word-count/files/fileOne.txt';
+var filePath2 = '../async-word-count/files/fileTwo.txt'
 var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
   // YOUR CODE HERE
+
+  getWordCount(filePathOne, function(err, data){
+
+  	getWordCount(filePathTwo, function(err, data2){
+  		var d = data + data2;
+  		callback(err, d);
+  	});
+
+  });
+  
 };
 
+getTotalWordCount(filePath1,filePath2, function(err, data){console.log(data);});
 module.exports = getTotalWordCount;
